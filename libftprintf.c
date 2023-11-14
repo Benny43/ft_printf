10825874309b06cdd9dd05d4c29f978406be5e7f
Bernardo Pereira <bernardopereira83@gmail.com>
2023-11-14 15:12:43 +0000

    fix .2

--- libftprintf.c
+++ libftprintf.c
@@ -6,7 +6,7 @@
 /*   By: benny <benny@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 01:10:36 by benny             #+#    #+#             */
-/*   Updated: 2023/10/30 01:56:10 by benny            ###   ########.fr       */
+/*   Updated: 2023/11/07 21:02:47 by benny            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,50 +23,69 @@ int	ft_printf(const char *format, ...)
 	unsigned long long int add;
 	char	buffer[20];
 	int		ptrdigit;
+	int		j;
+	float	decnbr;
+	char	decstr[20];
 
 	va_start (args, format);
-	while (*format)
+	i = 0;
+	j = 0;
+	while (format[i])
 	{
-		if (*format == '%' && *(format + 1) == 'c')
+		if (format[i] == '%' && format[i + 1] == 'c')
 		{
 			c = va_arg(args, int);
 			write(1, &c, 1);
-			format += 2;
+			i += 2;
 		}
-		if (*format == '%' && *(format + 1) == 's')
+		if (format[i] == '%' && format[i + 1] == 's')
 		{
 			slen = ft_strlen(va_arg(args, char *));
 			s = malloc(slen * sizeof(char));
 			s = va_arg(args, char *);
-			while (s[i])
+			while (s[j])
 			{
-				write(1, &s[i], 1);
+				write(1, &s[j], 1);
 				i++;
 			}
 			free(s);
-			format += 2;
+			i += 2;
 		}
-		if (*format == '%' && *(format + 1) == 'p')
+		if (format[i] == '%' && format[i + 1] == 'p')
 		{
 			ptr = va_arg(args, void *);
 			add = (unsigned long long int)ptr;
-			i = 0;
+			j = 0;
 			while (add >= 0)
 			{
 				ptrdigit = add % 16;
 				if (ptrdigit < 10)
-					buffer[i++] = '0' + ptrdigit;
+					buffer[j++] = '0' + ptrdigit;
 				else
-					buffer[i++] = 'a' + (ptrdigit - 10);
+					buffer[j++] = 'a' + (ptrdigit - 10);
 				add /= 16;
 			}
-			while (i > 0)
+			while (j > 0)
 			{
-				write(1, &buffer[i], 1);
+				write(1, &buffer[j], 1);
 			}
-			format += 2;
+			i += 2;
 		}
-		*format++;
+		if (format[i] == '%' && format[i + 1] == 'd')
+		{
+			decnbr = va_arg(args, float);
+			if (decnbr < 0)
+			{
+				write(1, "-", 1);
+				decnbr *= -1;
+			}
+			while ((long)decnbr > 0)
+			{
+				decstr[j] = (decnbr % 10) + '0';
+				decnbr /= 10;
+			}
+		}
+		i++;
 	}
 }
 
